a028deef63621bbaf9b763e3a214e8178ccabd17
assassinlife2003 <33318748+assassinlife2003@users.noreply.github.com>
2017-11-06 08:48:02 -0500

    actual project

---
+++ functions.py
@@ -0,0 +1,179 @@
+def white(t):
+    for times in range(100):
+        t.circle(800)
+        t.left(25)
+        t.color(255,255,255)
+        t.penup()
+        t.goto(0,0)
+        t.pendown()
+
+def whitestar(bob):
+    for times in range(110):
+        bob.penup()
+        bob.goto(0,0)
+        bob.pendown()
+        bob.begin_fill()
+        bob.color(255,0,0)
+        bob.forward(420)
+        bob.right(500)
+        bob.color(0,0,255)
+        bob.forward(495)
+        bob.forward(345)
+        bob.left(500)
+        bob.forward(350)
+        bob.right(500)
+        bob.color(255,255,255)
+        bob.end_fill()
+
+def redsquares(bob):
+    for times in range(100):
+        bob.penup()
+        bob.goto(0,0)
+        bob.pendown()
+        bob.begin_fill()
+        bob.color(255,0,0)
+        bob.forward(230)
+        bob.right(100)
+        bob.color(0,0,255)
+        bob.forward(305)
+        bob.forward(155)
+        bob.left(100)
+        bob.forward(160)
+        bob.right(100)
+        bob.color(255,0,0)
+        bob.end_fill()
+
+def blue(bob):
+    for times in range(100):
+        bob.circle(850)
+        bob.left(30)
+        bob.color(0,0,255)
+        bob.penup()
+        bob.goto(0,0)
+        bob.pendown()
+
+def redstar(bob):
+    for times in range(100):
+        bob.penup()
+        bob.goto(0,0)
+        bob.pendown()
+        bob.begin_fill()
+        bob.color(255,0,0)
+        bob.forward(310)
+        bob.right(100)
+        bob.color(0,255,255)
+        bob.forward(385)
+        bob.forward(235)
+        bob.left(100)
+        bob.forward(240)
+        bob.right(100)
+        bob.color(0,0,255)
+        bob.end_fill()
+
+def redline(bob):
+    for times in range(100):
+        bob.circle(900)
+        bob.left(35)
+        bob.color(255,0,0)
+        bob.penup()
+        bob.goto(0,0)
+        bob.pendown()
+
+def purplestar(bob):
+    for times in range(100):
+        bob.penup()
+        bob.goto(0,0)
+        bob.pendown()
+        bob.begin_fill()
+        bob.color(255,0,0)
+        bob.forward(270)
+        bob.right(100)
+        bob.color(0,255,255)
+        bob.forward(345)
+        bob.forward(195)
+        bob.left(100)
+        bob.forward(200)
+        bob.right(100)
+        bob.color(255,0,255)
+        bob.end_fill()
+
+def cyanline(bob):
+    for times in range(100):
+        bob.circle(950)
+        bob.left(40)
+        bob.color(0,255,255)
+        bob.penup()
+        bob.goto(0,0)
+        bob.pendown()
+
+def blackstar(bob):
+    for times in range(100):
+        bob.penup()
+        bob.goto(0,0)
+        bob.pendown()
+        bob.begin_fill()
+        bob.color(255,0,0)
+        bob.forward(290)
+        bob.right(100)
+        bob.color(0,0,255)
+        bob.forward(365)
+        bob.forward(215)
+        bob.left(100)
+        bob.forward(220)
+        bob.right(100)
+        bob.color(0,0,0)
+        bob.end_fill()
+
+def whitelinetwo(bob):
+    for times in range(100):
+        bob.circle(1000)
+        bob.left(45)
+        bob.color(255,255,255)
+        bob.penup()
+        bob.goto(0,0)
+        bob.pendown()
+
+def purpleline(bob):
+    for times in range(100):
+        bob.circle(1050)
+        bob.left(50)
+        bob.color(255,0,255)
+        bob.penup()
+        bob.goto(0,0)
+        bob.pendown()
+
+def whitestartwo(bob):
+    for times in range(110):
+        bob.penup()
+        bob.goto(0,0)
+        bob.pendown()
+        bob.begin_fill()
+        bob.color(255,0,0)
+        bob.forward(220)
+        bob.right(500)
+        bob.color(0,0,255)
+        bob.forward(245)
+        bob.forward(215)
+        bob.left(500)
+        bob.forward(220)
+        bob.right(500)
+        bob.color(255,255,255)
+        bob.end_fill()
+
+def whiteline3(bob):
+    for times in range(100):
+        bob.circle(1100)
+        bob.left(55)
+        bob.color(255,255,255)
+        bob.penup()
+        bob.goto(0,0)
+        bob.pendown()
+
+
+
+
+
+
+
+
+
---
+++ project test 5.py
@@ -0,0 +1,35 @@
+import turtle
+from functions import *
+
+turtle.colormode(255)
+turtle.bgcolor("black")
+
+bob=turtle.Turtle()
+bob.speed(0)
+turtle.tracer(0)
+
+white(bob)
+
+whitestar(bob)
+
+redsquares(bob)
+
+blue(bob)
+
+redstar(bob)
+
+redline(bob)
+
+purplestar(bob)
+
+cyanline(bob)
+
+blackstar(bob)
+
+whitelinetwo(bob)
+
+purpleline(bob)
+
+whitestartwo(bob)
+
+whiteline3(bob)
